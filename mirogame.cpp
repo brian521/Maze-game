781f9ce9107788a416d49835d76509d9851c13a1
이병헌 <brian52125@gmail.com>
2020-11-05 00:28:18 +0900

    종료설정

--- mirogame.cpp
+++ mirogame.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
 void SetMaze(char Maze[21][21])
@@ -11,18 +13,18 @@ void SetMaze(char Maze[21][21])
 	strcpy_s(Maze[5], "01000000011110000000");
 	strcpy_s(Maze[6], "01100000010000000000");
 	strcpy_s(Maze[7], "00100000011111111000");
-	strcpy_s(Maze[8], "00000000000000001113");
-	strcpy_s(Maze[9], "01100000000000000000");
-	strcpy_s(Maze[10], "21100000000000000000");
-	strcpy_s(Maze[11], "21100000000000000000");
-	strcpy_s(Maze[12], "21100000000000000000");
-	strcpy_s(Maze[13], "21100000000000000000");
-	strcpy_s(Maze[14], "21100000000000000000");
-	strcpy_s(Maze[15], "21100000000000000000");
-	strcpy_s(Maze[16], "21100000000000000000");
-	strcpy_s(Maze[17], "21100000000000000000");
-	strcpy_s(Maze[18], "21100000000000000000");
-	strcpy_s(Maze[19], "21100000000000000000");
+	strcpy_s(Maze[8], "00011110000000001000");
+	strcpy_s(Maze[9], "01110011111111111000");
+	strcpy_s(Maze[10], "01000000000000000000");
+	strcpy_s(Maze[11], "01111110000111111000");
+	strcpy_s(Maze[12], "00000011111100000000");
+	strcpy_s(Maze[13], "01111110000111111000");
+	strcpy_s(Maze[14], "01000000000000001000");
+	strcpy_s(Maze[15], "01111100111111111000");
+	strcpy_s(Maze[16], "00000101100000000000");
+	strcpy_s(Maze[17], "01111101000000000000");
+	strcpy_s(Maze[18], "01000001111111100000");
+	strcpy_s(Maze[19], "11000000000000111113");
 }
 
 void Output(char Maze[21][21])
@@ -54,7 +56,15 @@ int main() {
 
 	SetMaze(strMaze);
 
-	Output(strMaze);
+	while (true)
+	{
+		system("cls");
+		Output(strMaze);
+		cout << "w : 위 s : 아래 a : 왼쪽 d : 오른쪽 q : 종료 : ";
+		char	cInput = _getch();
 
+		if (cInput == 'q' || cInput == 'Q')
+			break;
+	}
 	return 0;
 }
